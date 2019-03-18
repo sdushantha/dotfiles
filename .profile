c8e0bc7861f10fe6530b6cfbc13875d23448ae82
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2019-03-18 12:53:46 +0100

    cleaned the while repo

--- .profile
+++
@@ -1,3 +0,0 @@
-export EDITOR="vim"
-export TERMINAL="termite"
-export BROWSER="firefox"
--- scripts/lockscreen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Pause mpd and send the pause key to all mpv videos:
-mpc pause >/dev/null 2>&1
-pauseallmpv >/dev/null 2>&1
-
-betterlockscreen -l
-
