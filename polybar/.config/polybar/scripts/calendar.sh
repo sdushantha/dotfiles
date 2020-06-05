178a4b1e7c58d2999e351fa28c4ad7decce1b71b
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2020-06-05 22:00:38 +0200

    polybar: added dots which shows workspace

--- polybar/.config/polybar/scripts/calendar.sh
+++ polybar/.config/polybar/scripts/calendar.sh
@@ -4,7 +4,7 @@ BAR_HEIGHT=22  # polybar height
 BORDER_SIZE=1  # border size from your wm settings
 YAD_WIDTH=222  # 222 is minimum possible value
 YAD_HEIGHT=193 # 193 is minimum possible value
-DATE="$(date +"%a %d")"
+DATE="$(date +"%a %d %H:%M")"
 
 case "$1" in
 --popup)
@@ -21,6 +21,6 @@ case "$1" in
         --title="yad-calendar" --borders=0 >/dev/null &
     ;;
 *)
-    echo " $DATE"
+    echo "$DATE"
     ;;
 esac
