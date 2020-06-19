60a989c334a8a912c03efa73394b14d1dc57a650
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2020-06-19 18:43:03 +0200

    screenshot.sh: fixed some issues, and cleaned up the code

--- sxhkd/.config/sxhkd/scripts/screenshot.sh
+++ sxhkd/.config/sxhkd/scripts/screenshot.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 #
 # by Siddharth Dushantha
-# Take a screenshot and then display the screenshot
-# using mpv as a little preview thing.
+#
+# dependencies: dunstify, maim, xclip and mpv
+# 
+# Take a screenshot and copy it to your clipboard.
+# Then send a notification saying that a screenshot
+# was taken and use the screenshot image as the icon
+# of the notification. If you click on the notification,
+# mpv will open the image so that you can take a look at it
+#
+
 FNAME="$HOME/pictures/screenshots/shot_$(date +%y-%m-%d_at_%H-%M-%S).png"
-if [[ $1 = "-s" ]];then
+
+if [ $1 == "-s" ];then
     # Select an area or click a window to take a screenshot of 
     # just the window
-    maim -s -u | xclip -selection clipboard -t image/png
-# v=$(maim -s -u) && printf "%s" "$v" | xclip -selection clipboard -t image/png
-    xclip -o -selection clipboard -t image/png > $FNAME
-    [[ $(dunstify -A "show,s" "screenshot saved" "$(basename $FNAME)" -i $FNAME) == "show" ]] && mpv $FNAME
+    maim -s -u "$FNAME"
 else
-    # Screenshot of whole screen
-    maim | xclip -selection clipboard -t image/png
-    xclip -o -selection clipboard -t image/png > $FNAME
-    [[ $(dunstify -A "show,s" "screenshot saved" "$(basename $FNAME)" -i $FNAME) == "show" ]] && mpv $FNAME
+    maim "$FNAME"
 fi
+
+EXIT_CODE=$?
+
+[ $EXIT_CODE -ne 0 ] && exit
+
+xclip -selection clipboard -t image/png < "$FNAME"
+
+[[ $(dunstify -A "show,s" "screenshot saved" "$(basename $FNAME)" -i $FNAME) == "show" ]] && mpv $FNAME
