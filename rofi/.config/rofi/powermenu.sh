0a77ec3cef911a3c4f9aa3b81e9c9cf1b46b204c
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2022-02-09 15:08:21 +0100

    rofi: made a few changes to the themes

--- rofi/.config/rofi/powermenu.sh
+++ rofi/.config/rofi/powermenu.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
-
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
+#
+# Original by @adi1090x modified by @sdushantha
+#
 
 dir="$HOME/.config/rofi/"
-rofi_command="rofi -theme $dir/powermenu.rasi"
-
-uptime=$(uptime -p | sed -e 's/up //g')
+rofi_command="rofi -theme $dir/themes/powermenu.rasi"
 
 # Options
 shutdown=""
@@ -20,7 +16,7 @@ logout=""
 # Variable passed to rofi
 options="$lock\n$shutdown\n$reboot\n$logout"
 
-chosen="$(echo -e "$options" | $rofi_command -p "$uptime" -dmenu -selected-row 0)"
+chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 0)"
 case $chosen in
 $shutdown)
   poweroff
@@ -30,18 +26,6 @@ $reboot)
   ;;
 $lock)
   betterlockscreen -l
-  ;;
-$suspend)
-  ans=$(confirm_exit &)
-  if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-    mpc -q pause
-    amixer set Master mute
-    systemctl suspend
-  elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-    exit 0
-  else
-    msg
-  fi
   ;;
 $logout)
     i3-msg exit
