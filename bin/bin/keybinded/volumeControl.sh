d087aa5b39e6e942a342001add4c04e4e6fbab27
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2023-01-17 23:01:49 +0100

    volumeControl: removed old code

--- bin/bin/keybinded/volumeControl.sh
+++ bin/bin/keybinded/volumeControl.sh
@@ -1,49 +1,15 @@
 #!/usr/bin/env bash
 
-# You can call this script like this:
-# $ ./volumeControl.sh up
-# $ ./volumeControl.sh down
-# $ ./volumeControl.sh mute
-
-# Script modified from these wonderful people:
-# https://github.com/dastorm/volume-notification-dunst/blob/master/volume.sh
-# https://gist.github.com/sebastiencs/5d7227f388d93374cebdf72e783fbd6a
-
-function get_volume {
-  amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
-}
-
-function is_mute {
-  amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
-}
-
-function send_notification {
-  iconSound="audio-volume-high"
-  iconMuted="audio-volume-muted"
-  if is_mute ; then
-    dunstify -i $iconMuted -r 2593 -u normal "mute"
-  else
-    volume=$(get_volume)
-    # Make the bar with the special character ─ (it's not dash -)
-    # https://en.wikipedia.org/wiki/Box-drawing_character
-    bar=$(seq --separator="─" 0 "$((volume / 5))" | sed 's/[0-9]//g')
-    # Send the notification
-    dunstify -i $iconSound -r 2593 -u low "    $bar"
-  fi
-}
-
 case $1 in
   up)
+    pactl set-sink-mute @DEFAULT_SINK@ 0
     pactl set-sink-volume @DEFAULT_SINK@ +5%
-    send_notification
     ;;
   down)
+    pactl set-sink-mute @DEFAULT_SINK@ 0
     pactl set-sink-volume @DEFAULT_SINK@ -5%
-    send_notification
     ;;
   mute)
-    # toggle mute
     pactl set-sink-mute @DEFAULT_SINK@ toggle
-    send_notification
     ;;
 esac
