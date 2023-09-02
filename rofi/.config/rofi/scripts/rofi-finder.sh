e383b820a418b598bf8a7ba369e6f87583013be5
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2023-09-02 12:00:39 +0200

    rofi: use fd for findings files

--- rofi/.config/rofi/scripts/rofi-finder.sh
+++ rofi/.config/rofi/scripts/rofi-finder.sh
@@ -1,7 +1,7 @@
-path="$1"
-
-selection=$(find "$path" -type f 2>/dev/null | \
+selection=$(fd . --hidden --type file $HOME 2>/dev/null | \
     sed "s;$HOME;~;" | \
-    rofi -sort -sorting-method fzf -disable-history -dmenu)
+    rofi -sort -sorting-method fzf -disable-history -dmenu -theme default-no-icon -no-custom -p "" | \
+    sed "s;\~;$HOME;"
+)
 
-open "$selection"
+open "$selection" 
