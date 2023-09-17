e89e0974baa5c37e72e22d2cbce23d85aa10c092
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2023-09-17 11:23:10 +0200

    rofi: added support for drag and drop in rofi-finder.sh
    New syntax: rofi-finder <opener>
    Where <opener> is the program that should open the file
    By using dragon-drop, we can launch a window to drag and drop the file
    into an application.
    
    There is a change things may change in the future where the drag and
    drop functionality will be directly inside the 'open' command

--- rofi/.config/rofi/scripts/rofi-finder.sh
+++ rofi/.config/rofi/scripts/rofi-finder.sh
@@ -1,7 +1,9 @@
+opener="$1"
+
 selection=$(fd . --hidden --type file $HOME 2>/dev/null | \
     sed "s;$HOME;~;" | \
-    rofi -sort -sorting-method fzf -disable-history -dmenu -theme default-no-icon -no-custom -p "" | \
+    rofi -sort -sorting-method fzf -disable-history -dmenu -i -theme default-no-icon -no-custom -p "" | \
     sed "s;\~;$HOME;"
 )
 
-open "$selection" 
+$opener "$selection" 
