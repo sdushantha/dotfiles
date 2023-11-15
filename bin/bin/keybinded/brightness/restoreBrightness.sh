cc7f14095b8a750fc46f63f037ad89847a65a60d
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2023-11-15 15:53:49 +0100

    cleaned up bin

--- bin/bin/keybinded/brightness/restoreBrightness.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Restore the brightness by taking the value in the file, brightness
-
-VALUE=$(cat $HOME/bin/keybinded/brightness/brightness)
-
-xbacklight -set $VALUE
--- bin/bin/keybinded/rofi-farge.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-for hexcode in $(find /tmp/farge -type f -exec basename {} .png \;); do
-    echo -en "#$hexcode\0icon\x1f/tmp/farge/$hexcode.png\n" 
-done 
