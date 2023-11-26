13f1ce73a39a6d36e503b41b649203304adecdd7
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2023-11-26 11:43:12 +0100

    nvim: removed bufferline keys
    I only use neovim for small edits for files. If different buffers are
    needed, then VSCode will be used

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -118,18 +118,6 @@ map("n", "<C-n>", ":e " .. vim.fn.expand('%:p:h') .. "/")
 -- Close current buffer
 map("n", "<C-w>", ":bp<CR>:bdelete #<CR>", {silent = true})
 
--- Go to Nth buffer
-map("", "<M-1>", "<CMD>lua require'bufferline'.go_to_buffer(1)<CR>")
-map("", "<M-2>", "<CMD>lua require'bufferline'.go_to_buffer(2)<CR>")
-map("", "<M-3>", "<CMD>lua require'bufferline'.go_to_buffer(3)<CR>")
-map("", "<M-4>", "<CMD>lua require'bufferline'.go_to_buffer(4)<CR>")
-map("", "<M-5>", "<CMD>lua require'bufferline'.go_to_buffer(5)<CR>")
-map("", "<M-6>", "<CMD>lua require'bufferline'.go_to_buffer(6)<CR>")
-map("", "<M-7>", "<CMD>lua require'bufferline'.go_to_buffer(7)<CR>")
-map("", "<M-8>", "<CMD>lua require'bufferline'.go_to_buffer(8)<CR>")
-map("", "<M-9>", "<CMD>lua require'bufferline'.go_to_buffer(9)<CR>")
-map("", "<M-0>", "<CMD>lua require'bufferline'.go_to_buffer(10)<CR>")
-
 -- Turn off highlighting
 map("n", "<Esc>", ":noh<CR>", {silent = true})
 
