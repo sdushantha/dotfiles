309f5d815b7cb50bf87fa7e9e039cf083744270a
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2024-05-10 09:53:52 +0000

    rofi: remove new line for hex value in farge

--- rofi/.config/rofi/scripts/rofi-farge.sh
+++ rofi/.config/rofi/scripts/rofi-farge.sh
@@ -2,4 +2,4 @@
 for file in $(ls -tl /tmp/farge | cut -d " " -f 9); do
     hex_code=$(echo $file | cut -d"." -f 1) 
     echo -en "#$hex_code\0icon\x1f/tmp/farge/$file\n" 
-done | rofi -dmenu | xclip -sel c
+done | rofi -dmenu | tr -d "\n" | xclip -sel c
