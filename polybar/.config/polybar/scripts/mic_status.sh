98a96b48b0f6bad45050c4f780db38c8236d6fbd
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2024-05-16 09:27:09 +0200

    polybar: added extra space after icon

--- polybar/.config/polybar/scripts/mic_status.sh
+++ polybar/.config/polybar/scripts/mic_status.sh
@@ -4,7 +4,7 @@ current_source=$(pactl info | grep "Default Source" | cut -f3 -d" ")
 mic_is_on=$(pactl list sources | grep -A 10 $current_source | grep "Mute: no" )
 
 if [ $? -eq 0 ]; then
-    printf %s ""
+    printf %s " "
 else
-    printf %s ""
+    printf %s " "
 fi
